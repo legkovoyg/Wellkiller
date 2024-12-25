7211cb888247b355ebeea475287f15f8a63393f3
German Legkovoy <147488090+legkovoyg@users.noreply.github.com>
2024-12-25 12:00:15 +0500

    123

--- rocksilencing/rocksilencing/settings.py
+++ rocksilencing/rocksilencing/settings.py
@@ -27,11 +27,10 @@ SECRET_KEY = "django-insecure-huvfj)&xrohe5+ba3(g_=1wh5r5u9g_45jh&luy_j**izy*j21
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["wellkilling.ru", "www.wellkilling.ru"]
 CSRF_TRUSTED_ORIGINS = [
-    "http://legkovoy.fvds.ru",
-    "http://wellkilling.fvds.ru",
-    "http://78.24.220.67",
+    "http://wellkilling.ru",
+    "http://www.wellkilling.ru",
 ]
 
 # Application definition
@@ -183,3 +182,15 @@ LOGGING = {
         },
     },
 }
+# Установите безопасные параметры для куки
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+# Настройте обработку SSL-заголовков
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# Дополнительные параметры безопасности
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # Один год
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
